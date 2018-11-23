1141d259037b633f936fb73cf229909c58a540bf
Stephan Herzog <stephan.herzog_ext@scout24.com>
2018-11-23 16:50:11 +0100

    add dist directory to make it installable

---
+++ dist/schemas/optionsDefaults.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+const optionsDefaults = {
+  attributeNames: {
+    styleName: 'className'
+  },
+  handleMissingStyleName: 'throw'
+};
+var _default = optionsDefaults;
+exports.default = _default;
+//# sourceMappingURL=optionsDefaults.js.map
